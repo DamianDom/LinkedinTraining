1565c3d02456c34feb7d530e200b3e99c15e3796
Damian <damianndomagala@gmail.com>
2021-06-13 19:14:54 +0200

    divided code into methods and tried cast for static / non static methods

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -4,30 +4,45 @@ package com.example.java;
 // CTRL+K - commit
 // CTRL+SHIFT+K - push
 public class Main {
-   public static void main(String[] args) {
-/*     System.out.println("Hello There !");
-    String aString = "Damian";
-    System.out.println("Hello there " + aString);
 
-    double doubleValue = 156.5d;
-    Double doubleObj = new Double(doubleValue);
-    byte byteValue = doubleObj.byteValue();
-    int intValue = doubleObj.intValue();
-    float floatValue = doubleObj.floatValue();
-    String stringValue = doubleObj.toString();
-        System.out.println("DOUBLE: " + doubleObj);
-        System.out.println("BYTE: " + byteValue);
-        System.out.println("INT: " + intValue);
-        System.out.println("FLOAT: " + floatValue);
-        System.out.println("STRING: " + stringValue);*/
+    void primitivesCast(){
+        System.out.println("Hello There !");
+        String aString = "Damian";
+        System.out.println("Hello there " + aString);
 
-    byte b = 127;
+        double doubleValue = 156.5d;
+        Double doubleObj = new Double(doubleValue);
+        byte byteValue = doubleObj.byteValue();
+        int intValue = doubleObj.intValue();
+        float floatValue = doubleObj.floatValue();
+        String stringValue = doubleObj.toString();
+            System.out.println("DOUBLE: " + doubleObj);
+            System.out.println("BYTE: " + byteValue);
+            System.out.println("INT: " + intValue);
+            System.out.println("FLOAT: " + floatValue);
+            System.out.println("STRING: " + stringValue);
+    }
+
+    static void maxValues(){
+        byte b = 127;
         System.out.println("Byte:" + b);
 
         if( b < Byte.MAX_VALUE){
-        b++;
-    }
+            b++;
+        }
         System.out.println("Byte" + b);
-}
-
     }
+
+   public static void main(String[] args) {
+        maxValues(); // invoked static method
+        System.out.println("\n");
+        Main mn = new Main();
+        mn.primitivesCast(); // invoked non static method
+
+
+
+   }
+
+
+
+}
