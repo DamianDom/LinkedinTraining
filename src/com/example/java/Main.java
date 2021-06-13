8edde7ab59e9ec76db822d4198b6e03ccde708b2
Damian <damianndomagala@gmail.com>
2021-06-13 18:55:47 +0200

    primitives cast

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -8,5 +8,20 @@ public class Main {
     System.out.println("Hello There !");
     String aString = "Damian";
     System.out.println("Hello there " + aString);
+
+    double doubleValue = 156.5d;
+    Double doubleObj = new Double(doubleValue);
+    byte byteValue = doubleObj.byteValue();
+    int intValue = doubleObj.intValue();
+    float floatValue = doubleObj.floatValue();
+    String stringValue = doubleObj.toString();
+        System.out.println("DOUBLE: " + doubleObj);
+        System.out.println("BYTE: " + byteValue);
+        System.out.println("INT: " + intValue);
+        System.out.println("FLOAT: " + floatValue);
+        System.out.println("STRING: " + stringValue);
+
+
+
     }
 }
