0dcd7ab4b644cef5c41ef19a7a1f5b32abb233b9
Damian <damianndomagala@gmail.com>
2021-06-21 18:26:35 +0200

    on String methods

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -1,6 +1,9 @@
 package com.example.java;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
 
 // CTRL+Q - java documentation
 // CTRL+K - commit
@@ -11,7 +14,7 @@ public class Main {
     static boolean bDef; // boolean without value always start as FALSE
 
 
-    void primitivesCast(){
+    void primitivesCast() {
         System.out.println("Hello There !");
         String aString = "Damian";
         System.out.println("Hello there " + aString);
@@ -22,24 +25,24 @@ public class Main {
         int intValue = doubleObj.intValue();
         float floatValue = doubleObj.floatValue();
         String stringValue = doubleObj.toString();
-            System.out.println("DOUBLE: " + doubleObj);
-            System.out.println("BYTE: " + byteValue);
-            System.out.println("INT: " + intValue);
-            System.out.println("FLOAT: " + floatValue);
-            System.out.println("STRING: " + stringValue);
+        System.out.println("DOUBLE: " + doubleObj);
+        System.out.println("BYTE: " + byteValue);
+        System.out.println("INT: " + intValue);
+        System.out.println("FLOAT: " + floatValue);
+        System.out.println("STRING: " + stringValue);
     }
 
-    static void maxValues(){
+    static void maxValues() {
         byte b = 127;
         System.out.println("Byte:" + b);
 
-        if( b < Byte.MAX_VALUE){
+        if (b < Byte.MAX_VALUE) {
             b++;
         }
         System.out.println("Byte" + b);
     }
 
-    static void doublePrecise(){
+    static void doublePrecise() {
         double value = .012;
         double pSum = value + value + value;
 
@@ -53,7 +56,7 @@ public class Main {
         System.out.println("Sum of BigDecimals: " + bSum.toString());
     }
 
-    static void convertingNumbers(){
+    static void convertingNumbers() {
         int intValue1 = 56;
         int intValue2 = intValue1;
         System.out.println("The 2nd value is " + intValue2);
@@ -73,7 +76,7 @@ public class Main {
         System.out.println("Double to int: " + intValue4);
     }
 
-    static void mathLibrary(){
+    static void mathLibrary() {
         int intValue1 = 56;
         int intValue2 = 42;
 
@@ -100,28 +103,28 @@ public class Main {
         System.out.println("Absolute: " + absValue);
     }
 
-    static void booleans(){
+    static void booleans() {
 
-            boolean b1 = true;
-            boolean b2 = false;
+        boolean b1 = true;
+        boolean b2 = false;
 
-            System.out.println("The value of b1 is: " + b1);
-            System.out.println("The value of b2 is: " + b2);
-            System.out.println("The value of bDef is: " + bDef);
+        System.out.println("The value of b1 is: " + b1);
+        System.out.println("The value of b2 is: " + b2);
+        System.out.println("The value of bDef is: " + bDef);
 
-            boolean b3 = !b1;
-            System.out.println("The value of b3 is: " + b3);
+        boolean b3 = !b1;
+        System.out.println("The value of b3 is: " + b3);
 
-            int i1 = 0;
-            boolean b4 = (i1 != 0);
-            System.out.println("The value of b4 is: " + b4);
+        int i1 = 0;
+        boolean b4 = (i1 != 0);
+        System.out.println("The value of b4 is: " + b4);
 
-            String sBoolean = "true";
-            boolean parsed = Boolean.parseBoolean(sBoolean);
-            System.out.println("Parsed: " + parsed);
+        String sBoolean = "true";
+        boolean parsed = Boolean.parseBoolean(sBoolean);
+        System.out.println("Parsed: " + parsed);
     }
 
-    static void chars(){
+    static void chars() {
 
         char c1 = '1';
         char c2 = '2';
@@ -141,25 +144,149 @@ public class Main {
         System.out.println(Character.toUpperCase(a3));
     }
 
-   public static void main(String[] args) {
-        maxValues(); // invoked static method
+    static void charToString() {
+        char[] chars = {'H', 'e', 'l', 'l', 'o', '!'};
+        String s = new String(chars);
+        System.out.println(s);
+    }
+
+    static void string() {
+        // those 2 below are the same
+        String s1 = "This is a String";
+        String s2 = new String("This also a string");
+        String s3 = "Shirt size: ";
+        String s4 = "M";
+        String s5 = s3 + s4 + "Qty: " + 4;
+        System.out.println(s5);
+
+        char[] chars = {'H', 'e', 'l', 'l', 'o', '!'};
+        String s6 = new String(chars);
+        System.out.println(s6);
+
+        char[] chars2 = s6.toCharArray();
+        for (char c : chars2) {
+            System.out.println(c);
+        }
+    }
+
+    private static void primitiveToString() {
+        int intValue = 42;
+        String fromInt = Integer.toString(intValue);
+
+        boolean boolValue = true;
+
+        long longValue = 10_000_000;
+        NumberFormat formatter = NumberFormat.getNumberInstance();
+        String formatted = formatter.format(longValue);
+        System.out.println(formatted);
+    }
+
+    private static void stringBuilding() {
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1 + ", " + str2 + "1";
+        System.out.println(str3);
+
+        StringBuilder sb = new StringBuilder("Hello")
+                .append(", ")
+                .append("World")
+                .append("!");
+        System.out.println(sb);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value: ");
+        String input = scanner.nextLine();
+        System.out.println(input);
+
+        sb.delete(0, sb.length());
+        for (int i = 0; i < 3; i++) {
+            input = scanner.nextLine();
+            sb.append(input + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void compareStrings() {
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        if (str1 == str2) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
+        String str3 = "hello";
+        if (str1 == str3) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
+        String part1 = "Hello ";
+        String part2 = "WORLD";
+        String str4 = part1 + part2;
+        String str5 = "Hello World";
+        if (str4.equalsIgnoreCase(str5)) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+    }
+
+    private static void numericToString(){
+
+        Locale locale = new Locale("pl", "PL");
+
+        double doubleValue = 1_234_567.89;
+
+        NumberFormat numF = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        NumberFormat curF = NumberFormat.getCurrencyInstance(locale);
+        System.out.println("Currency: " + curF.format(doubleValue));
+
+        NumberFormat intF = NumberFormat.getIntegerInstance();
+        System.out.println("Integer: " + intF.format(doubleValue));
+    }
+
+    private static void parseString(){
+        String s1 = "Welcome to California!";
+        System.out.println("Length of string: " + s1.length());
+
+        int position = s1.indexOf("California");
+        System.out.println("Position of substring: " + position);
+
+        String sub = s1.substring(11);
+        System.out.println(sub);
+
+        String s2 = "Welcome!         ";
+        int len1 = s2.length();
+        System.out.println(len1);
+        String s3 = s2.trim();
+        System.out.println(s3.length());
+    }
+
+    public static void main(String[] args) {
+        /* maxValues(); // invoked static method
         System.out.println("********************************************************************************************");
         Main mn = new Main();
         mn.primitivesCast(); // invoked non static method
         System.out.println("********************************************************************************************");
         doublePrecise();
-        System.out.println("********************************************************************************************");
         convertingNumbers();
-        System.out.println("********************************************************************************************");
         mathLibrary();
-        System.out.println("********************************************************************************************");
         booleans();
-        System.out.println("********************************************************************************************");
         chars();
-        System.out.println("********************************************************************************************");
-
-   }
-
+        charToString();
+        string();
+        primitiveToString();
+        stringBuilding();
+        compareStrings();*/
+        numericToString();
+        parseString();
+    }
 
 
 }
